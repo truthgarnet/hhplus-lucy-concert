0689b479ecb111134083079776afeb50f7c6d73a
truthgarnet <57488270+truthgarnet@users.noreply.github.com>
2024-10-24 23:10:02 +0900

    [UPDATE] 엔티티 구조 변경

--- src/main/java/com/hhplus/concert/concertItem/infra/ConcertItemEntity.java
+++ src/main/java/com/hhplus/concert/concertItem/infra/ConcertItemEntity.java
@@ -3,22 +3,21 @@ package com.hhplus.concert.concertItem.infra;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Date;
-import java.util.List;
 
 @Entity
 @Table(name = "concert_item")
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class ConcertItemEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long concertItemId;
-    private List<Long> seatIds;
+    private Long seatId;
     private LocalDate reservationStart;
     private LocalDate reservationEnd;
     private LocalDate concertStart;
--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -5,7 +5,7 @@ INSERT INTO seat (concert_item_id, all_seat, remainder_seat)
 VALUES (1, 50, 50);
 
 
-INSERT INTO concert_item (reservation_start, reservation_end, concert_start)
-VALUES ('2024-12-15 19:00:00', '2024-12-15 22:00:00', '2024-10-01 09:00:00');
+INSERT INTO concert_item (reservation_start, reservation_end, concert_start, concert_id, seat_id)
+VALUES ('2024-12-15 19:00:00', '2024-12-15 22:00:00', '2024-10-01 09:00:00', 1, 1);
 
 
